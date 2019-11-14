12aee2871b0e284b5c7218eee76a78cf08a9e1d3
ahais777 <55410260+ahais777@users.noreply.github.com>
2019-11-14 15:35:25 -0500

    got rid of commented-out code at top of main.py

--- main.py
+++ main.py
@@ -14,33 +14,6 @@ from sklearn.metrics import mean_squared_error
 from get_schedule import *
 from get_team_info import *
 
-# import nba_scraper.nba_scraper as ns
-
-# BASKETBALL REFERENCE AND BEAUTIFUL SOUP FOR INDIVIDUAL PLAYER STATS
-# NBA season we will be analyzing
-year = 2019
-# URL page we will scraping (see image above)
-url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html".format(year)
-# this is the HTML from the given URL
-html = urlopen(url)
-soup = BeautifulSoup(html, "lxml")
-
-# use findALL() to get the column headers
-soup.findAll('tr', limit=2)
-# use getText()to extract the text we need into a list
-headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-# exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
-headers = headers[1:]
-#print(headers)
-
-# avoid the first header row
-rows = soup.findAll('tr')[1:]
-player_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
-
-stats = pd.DataFrame(player_stats, columns = headers)
-# print(stats.head(10))
-# print('\n')
-# print(len(stats.columns))
 
 team_stats = get_team_stats()[0]
 opp_stats = get_team_stats()[1]
